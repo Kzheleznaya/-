28c790a4d1f37e587aeeaee3222550f04e217b58
Wydjemko <k7259625@gmail.com>
2022-08-30 19:04:22 +0300

    Работа с числами

---
+++ chisla.py
@@ -0,0 +1,25 @@
+chislo1 = 1
+print(chislo1 ** 100)
+chislo2 = 2
+chislo3 = 16
+print(pow(chislo2, chislo3))  # возведение в степень через функцию pow
+chislo4 = 1000
+print(chislo2 - chislo4)
+print(chislo2 + chislo2 * chislo2)
+chislo5 = 60
+str1 = "3"
+print(chislo5 / int(str1))
+import math
+
+print(math.sqrt(144))
+x = 2 + 5 * 14
+print("x=", x)
+# 66-x=11
+x = 66 - 11
+print("x=",x )
+# корень х+8=16
+'''корень(х)=16-8
+корень(х)=8'''
+x=8**2
+print("x=",x)
+
